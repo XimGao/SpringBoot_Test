055264b9e0c38aed0c647291635a0e84f51aa1e7
Your Name <you@example.com>
2021-08-24 22:13:16 +0800

    first commit

---
+++ src/test/java/com/gxm/up/springboot_test/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxm.up.springboot_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
